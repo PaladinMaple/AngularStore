db176863b1fe04d175cd5d2fe0f887728c225939
unknown <PaladinMaple>
2021-07-31 19:59:55 -0700

    componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Product} from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'store';
+  items = ['depresión', 'esquizofrenia', 'ansiedad'];
+
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/leisure.jpg',
+      title: 'Leisure',
+      price: 80,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/images/modernLifeIsRubbish.jpg',
+      title: 'Modern Life Is Rubbish',
+      price: 80,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/images/parklife.jpg',
+      title: 'Parklife',
+      price: 80,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'assets/images/thinkTank.jpg',
+      title: 'Think Tank',
+      price: 80,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'assets/images/magicWhip.jpg',
+      title: 'Magic Whip',
+      price: 80,
+      description: 'bla bla bla bla bla'
+    }
+  ];
+
+  addItem(){
+    this.items.push('nuevo item');
+  }
+
+  deleteItem(index: number){
+    this.items.splice(index, 1);
+  }
+
+  clickProduct(id: number){
+    console.log('product from father');
+    console.log(id);
+  }
 }
